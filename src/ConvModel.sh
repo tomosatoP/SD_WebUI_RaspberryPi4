29abfe127849017ef660f24eb572e479d7503999
tomosatoP <120862812+tomosatoP@users.noreply.github.com>
2023-06-24 11:22:09 +0900

    Add files via upload

---
+++ src/ConvModel.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+if [ $# -ne 2 ]; then
+  echo "Error: usage - sh $0 <model_id> <output_dir>"
+  exit 1
+elif [ ! -e $2 ]; then
+  echo "Error: No $2 was found."
+  exit 2
+fi
+
+python3 download_diffusers.py --model_id $1
+
+if [ $? -ne 0 ]; then
+  exit 3
+fi
+
+if [ ! -e $2/$1 ]; then
+  echo "mkdir -p $2/$1"
+  mkdir -p $2/$1
+fi
+
+echo 'Combine safetensors files into one.'
+python3 convert_diffusers_to_original_stable_diffusion.py \
+  --model_path model_fp16/$1 \
+  --checkpoint_path $2/$1/model_fp16.safetensors \
+  --use_safetensors \
+  --half
+
+if [ $? -eq 0 ]; then
+  echo 'Completed.'
+  echo `ls -lh $2/$1/model_fp16.safetensors`
+  echo 'Delete folders that are no longer needed.'
+  rm -rf model/$1
+  rm -rf model_fp16/$1
+else
+  echo 'Error: Could not combine files into one.'
+  exit 4
+fi
+
+exit 0
---
+++ src/download_diffusers.py
@@ -0,0 +1,55 @@
+"""Convert a pickle format model to a safetensor format model.
+
+sudo apt install python3-pip
+sudo -H python3 -m pip install -U pip setuptools wheel
+sudo -H python3 -m pip install torch diffusers transformsers accelerate
+"""
+
+from diffusers import StableDiffusionPipeline
+import torch
+import argparse
+import sys
+
+
+# model_id = "stabilityai/stable-diffusion-2-1"
+
+
+def convert(model_id: str) -> bool:
+    try:
+        print(f"Start downloading the model: {model_id}")
+
+        pipe = StableDiffusionPipeline.from_pretrained(
+            model_id,
+            cache_dir=f"model/{model_id}",
+            # revision="fp16",
+            # torch_dtype=torch.float16
+        )
+
+        print("converting pickles to safetensors ...")
+
+        pipe.save_pretrained(f"model_fp16/{model_id}", safe_serialization=True)
+
+    except:
+        print("Error: download failed and will be interrupted.")
+        return False
+
+    else:
+        print("Download completed.")
+        return True
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Download model in safetensors format."
+    )
+    parser.add_argument(
+        "--model_id",
+        required=True,
+        type=str,
+        nargs="?",
+        help="Path to the model to convert.",
+    )
+
+    args = parser.parse_args()
+
+    sys.exit(0) if convert(args.model_id) else sys.exit(1)
