d46b992b2ab66bed81316b26e5e944020717e1e9
tomosatoP <120862812+tomosatoP@users.noreply.github.com>
2023-08-09 14:08:32 +0900

    Update download_diffusers.py

--- hf-diffusers-model/download_diffusers.py
+++ hf-diffusers-model/download_diffusers.py
@@ -12,22 +12,24 @@ import sys
 
 
 # model_id = "stabilityai/stable-diffusion-2-1"
+torch.backends.cuda.matmul.allow_tf32 = True
 
-
-def convert(model_id: str) -> bool:
+def convert(model_id: str, revision: str, output_dir: str) -> bool:
     try:
         print(f"Start downloading the model: {model_id}")
 
         pipe = StableDiffusionPipeline.from_pretrained(
             model_id,
-            cache_dir=f"model/{model_id}",
-            # revision="fp16",
-            # torch_dtype=torch.float16
+            cache_dir=f"cache/{model_id}",
+            revision=revision,
+            # torch_dtype=torch.float16,
+            # revision="main",
+            # torch_dtype=torch.float32,
         )
 
         print("converting pickles to safetensors ...")
 
-        pipe.save_pretrained(f"model_main/{model_id}", safe_serialization=True)
+        pipe.save_pretrained(f"{output_dir}/{model_id}", safe_serialization=True)
 
     except:
         print("Error: download failed and will be interrupted.")
@@ -42,6 +44,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Download model in safetensors format."
     )
+
     parser.add_argument(
         "--model_id",
         required=True,
@@ -50,6 +53,22 @@ if __name__ == "__main__":
         help="Path to the model to convert.",
     )
 
+    parser.add_argument(
+        "--revision",
+        required=True,
+        type=str,
+        nargs="?",
+        help="Model revision, e.g., 'main'.",
+    )
+
+    parser.add_argument(
+        "--output_dir",
+        required=True,
+        type=str,
+        nargs="?",
+        help="Path to output.",
+    )
+
     args = parser.parse_args()
 
-    sys.exit(0) if convert(args.model_id) else sys.exit(1)
+    sys.exit(0) if convert(args.model_id, args.revision, args.output_dir) else sys.exit(1)
